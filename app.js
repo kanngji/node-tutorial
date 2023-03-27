10c44459d0d648d8016bd001879e1c708ec252dc
kanngji <alleneffort1994@gmail.com>
2023-03-27 20:59:20 +0900

    fs modules

--- app.js
+++
@@ -1,8 +0,0 @@
-const names = require("./study/name");
-const sayHi = require("./study/util");
-const data = require("./study/alternative-flavor");
-require("./study/mind-grenade");
-
-// sayHi("susan");
-// sayHi(names.jhon);
-// sayHi(names.peter);
